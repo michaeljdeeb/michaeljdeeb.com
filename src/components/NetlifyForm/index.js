f30826d601b70451c7a5fcb68681a929e1563e1f
Michael Deeb <git@michaeljdeeb.com>
2018-03-17 11:39:33 -0700

    Honeypot for the bots

--- src/components/NetlifyForm/index.js
+++ src/components/NetlifyForm/index.js
@@ -5,10 +5,13 @@ import styles from './index.module.css';
 const NetlifyForm = () => (
   <div className={styles.NetlifyForm}>
     <h3>Email</h3>
-    <form className={styles.form} name="Contact" method="POST" data-netlify="true">
+    <form className={styles.form} name="Contact" method="POST" data-netlify="true" data-netlify-honeypot="Phone">
       <div className={styles.inputWrapper}>
         <label className={styles.label} htmlFor="Name">
           <input autoComplete="name" className={styles.input} type="text" name="Name" placeholder="Name" />
+        </label>
+        <label className={styles.phone} htmlFor="Phone">
+          <input autoComplete="tel" className={styles.input} type="text" name="Phone" placeholder="Phone" />
         </label>
         <label htmlFor="Email">
           <input autoComplete="email" className={styles.input} type="text" name="Email" placeholder="Email" />
